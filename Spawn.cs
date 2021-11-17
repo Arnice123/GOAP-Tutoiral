f4a8660c1a8a0744642d5c50de518765835630af
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2021-11-17 09:36:10 -0800

    Add files via upload

---
+++ Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject patientPrefab;
+    public int numPatient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numPatient; i++)
+        {
+            Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+        }
+
+        Invoke("SpawnPatient", 5);
+    }
+
+    void SpawnPatient()
+    {
+        Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+
+        Invoke("SpawnPatient", Random.Range(7, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
